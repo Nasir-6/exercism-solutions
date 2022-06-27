48ea40a1d8b58eef1c6d12dd39a567568fe195e7
Nasir <nasir-islam@hotmail.com>
2022-06-27 20:46:26 +0100

    add twoSum

---
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  const num1 = Number(String(array1).replace(/,/g, ''));
+  const num2 = Number(String(array2).replace(/,/g, ''));
+  return num1 + num2;
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  throw new Error('Implement the luckyNumber function');
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  throw new Error('Implement the errorMessage function');
+}
