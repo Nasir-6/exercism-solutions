83c60e2baad2da613051060e6037e330ba1bc06b
Nasir <nasir-islam@hotmail.com>
2022-06-29 23:41:28 +0100

    add cookingStatus

---
+++ javascript/lasagna-master/lasagna-master.js
@@ -0,0 +1,18 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+
+export function cookingStatus(timeLeft){
+    if(timeLeft === 0) return 'Lasagna is done.'
+    if(timeLeft === undefined) return 'You forgot to set the timer.'
+    return 'Not done, please wait.'
+}
