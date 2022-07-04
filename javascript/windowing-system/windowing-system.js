9a500ff362892d8d307b79beb89a675bd7232781
Nasir <nasir-islam@hotmail.com>
2022-07-04 20:26:33 +0100

    add ProgramWindow class constructor and resize fn

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -31,3 +31,32 @@ export class Position {
         this.y = newY;
     }
 }
+
+export class ProgramWindow{
+    constructor(){
+        this.screenSize = new Size(800, 600);
+        this.size = new Size();
+        this.position = new Position();
+    }
+
+    /**
+ * Check if stack contains odd-value card
+ *
+ * @param {Size} newSize
+ *
+ * @returns {void}
+ */
+    resize(newSize){
+        if(newSize.width < 1) newSize.width = 1;
+        if(newSize.height < 1) newSize.height = 1;
+
+        let remainingWidth = this.screenSize.width - this.position.x;
+        let remainingHeight = this.screenSize.height - this.position.y;
+
+        if(newSize.width > remainingWidth) newSize.width = remainingWidth;
+        if(newSize.height > remainingHeight) newSize.height = remainingHeight;
+
+        this.size.width = newSize.width;
+        this.size.height = newSize.height;
+    }
+}
